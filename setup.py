3b2d6300957a8312b81d9a97e7a2fbace57f6f22
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-25 17:46:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("apt update")
-    os.system("apt upgrade")
+    
     os.system("apt install python")
     os.system("apt install python3")
     os.system("pip2 install requests")
-
+    os.system("git pull")
 elif c == "1":
 
-     os.system("apt update")
-    os.system("apt upgrade")
+    
     os.system("apt install python")
     os.system("apt install python3")
     os.system("pip2 install requests")
+    os.system("git pull")
 
 print("Done.")
